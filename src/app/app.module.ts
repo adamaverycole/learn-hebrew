385a7ef0b77aa04d17de7dd742006af1eb0169b7
adamaverycole <adamaverycole@gmail.com>
2022-02-25 15:28:53 -0600

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +11,7 @@ import { WordDetailComponent } from './word-detail/word-detail.component';
 import { EnumToStringPipe } from './enum-to-string.pipe';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgbModule],
   declarations: [
     AppComponent,
     HelloComponent,
